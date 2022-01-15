ce3c42edd5fb888d310db176652007d0f15cde1c
Maxim Malchenko <maxim.malchenko@gmail.com>
2022-01-15 23:37:33 +0100

    card components  after first  review with Yehor

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
 import "./styles/main.scss";
 
 // This code work only with a <button> tag in HTML,  doesn't work with <a></a>. Why?
-
-const cardBtn = document.querySelector(".card__btn");
-const backSide = document.querySelector(".card__back-side");
-const frontSide = document.querySelector(".card__image");
-const bookmark = document.querySelector(".card__bookmark");
+//for each card
+//quarySelectorAll - node list
+const card = document.querySelector(".card");
+const cardBtn = card.querySelector(".card__btn");
+const backSide = card.querySelector(".card__back-side");
+const frontSide = card.querySelector(".card__image");
+const bookmark = card.querySelector(".card__bookmark");
 
 let cardFlip = false;
-
+//classList.toggle try this one
 cardBtn.addEventListener("click", () => {
   if (!cardFlip) {
     backSide.classList.add("card__back-side--show");
     frontSide.classList.add("card__image--show");
     bookmark.classList.add("card__bookmark--toggle");
+    //try to use .textContent
     cardBtn.innerHTML = "hide ingridients";
     cardFlip = true;
   } else {
